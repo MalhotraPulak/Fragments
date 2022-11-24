c029a4dd7aa06b177c174bafe2209bef005b4a27
shreyas <shpradhan12@gmail.com>
2022-11-25 03:52:31 +0530

    Added highlight box

---
+++ Assets/HighlightBox.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HighlightBox : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public GameObject parentObject;
+    public GameObject targetObject;
+    public float triggerDistance = 5f;
+
+    SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool additionalCheck = CheckBodyParts();
+
+        if (Vector2.Distance(targetObject.transform.position, parentObject.transform.position) < triggerDistance && additionalCheck)
+        {
+
+            if(!rend.enabled)
+            {
+                rend.enabled = true;
+            }
+        }
+        else
+        {
+            if(rend.enabled)
+            {
+                rend.enabled = false;
+            }
+        }
+    }
+
+    public bool CheckBodyParts()
+    {
+        PhysicsObject script = null;
+
+        if(parentObject.GetComponent<LeftArm>() != null)
+        {
+            script = parentObject.GetComponent<LeftArm>();
+        }
+        else if(parentObject.GetComponent<RightArm>() != null)
+        {
+            script = parentObject.GetComponent<RightArm>();
+        }
+        else if(parentObject.GetComponent<Legs>() != null)
+        {
+            script = parentObject.GetComponent<Legs>();
+        }
+        else
+        {
+            return true;
+        }
+
+        print(script.velocity);
+        if(Mathf.Abs(script.velocity.x) < 0.001 && Mathf.Abs(script.velocity.y) < 0.001 && script.grounded)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
+
