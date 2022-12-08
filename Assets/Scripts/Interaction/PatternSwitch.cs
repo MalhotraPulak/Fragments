a106049e6d7a90c8bd7819bdccb9ac10176daf6d
shreyas <shpradhan12@gmail.com>
2022-12-09 00:45:49 +0530

    fixed sprites. game playable from start to end

--- Assets/Scripts/Interaction/PatternSwitch.cs
+++ Assets/Scripts/Interaction/PatternSwitch.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 public class PatternSwitch : Switch
 {
 
+    bool isPressed = false;
     public int switchId = 0;
+
+    public Sprite pressedButtonSprite;
+    public Sprite unpressedButtonSprite;
+
+    void Update()
+    {
+        if(isPressed)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = pressedButtonSprite;
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = unpressedButtonSprite;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -13,10 +29,20 @@ public class PatternSwitch : Switch
         // check type and toggle door
         if (col.gameObject.tag == "attackCollider" || col.gameObject.tag == "Legs")
         {
+            isPressed = true;
             foreach (GameObject door in doors)
             {
                 door.GetComponent<Door>().RegisterPatternSwitch(switchId);
             }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        // check type and toggle door
+        if (col.gameObject.tag == "attackCollider" || col.gameObject.tag == "Legs")
+        {
+            isPressed = false;
         }
     }
 }
--- Assets/Scripts/Interaction/PressSwitch.cs
+++ Assets/Scripts/Interaction/PressSwitch.cs
@@ -6,11 +6,27 @@ public class PressSwitch : Switch
 {
     bool isPressed = false;
 
+    public Sprite pressedButtonSprite;
+    public Sprite unpressedButtonSprite;
+
+    void Update()
+    {
+        if(isPressed)
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = pressedButtonSprite;
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = unpressedButtonSprite;
+        }
+    }
+
     void OnTriggerStay2D(Collider2D col)
     {
 
         if (col.gameObject.tag == "Floppy" || col.gameObject.tag == "Legs" )
         {
+            isPressed = true;
             foreach (GameObject door in doors)
             {
                 Door doorScript = door.GetComponent<Door>();
@@ -21,10 +37,10 @@ public class PressSwitch : Switch
 
     void OnTriggerExit2D(Collider2D col)
     {
-
         // check type and toggle door
         if (col.gameObject.tag == "Floppy" || col.gameObject.tag == "Legs")
         {
+            isPressed = false;
             foreach (GameObject door in doors)
             {
                 Door doorScript = door.GetComponent<Door>();
